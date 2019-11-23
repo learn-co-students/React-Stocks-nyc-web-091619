dabb5010d410ff86e83e815db36c237bb5ede140
maryfarner18 <maryfarner@gmail.com>
2019-11-23 14:30:27 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,92 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    allStocks: [],
+    portfolioStocks: [],
+    priceSort: false,
+    industrySort: "all", // Tech, 
+    alphaSort: false
+  }
+
+  sortStocks = () =>{
+    let sorted = [...this.state.allStocks]
+    if(this.state.alphaSort){
+      sorted.sort((a,b) => a.name > b.name ? 1 : -1)
+    }
+
+    if(this.state.priceSort){
+      sorted.sort((a,b)=> a.price - b.price)
+    }
+    if(this.state.industrySort !== "all"){
+      sorted = sorted.filter(stock => stock.type === this.state.industrySort)
+    }
+    return sorted
+  }
+
+  changePriceSort = () =>{
+    this.setState({
+      priceSort: !this.state.priceSort,
+      alphaSort: false
+    })
+  }
+
+  changeAlphaSort = () =>{
+    this.setState({
+      alphaSort: !this.state.alphaSort,
+      priceSort: false
+    })
+  }
+
+  changeIndustrySort = (sort) =>{
+    this.setState({
+      industrySort: sort
+    })
+  }
+
+  
+  handleBuy = (stock) => {
+    console.log("adding, ", stock, " to portfolio")
+    this.setState({
+      portfolioStocks: [...this.state.portfolioStocks, stock]
+    })
+  }
+  
+  handleSell = (stock) => {
+    console.log("removing", stock, " from portfolio")
+    let index = this.state.portfolioStocks.findIndex(aStock => aStock === stock)
+    let newPort = [...this.state.portfolioStocks]
+    newPort.splice(index, 1)
+    this.setState({
+      portfolioStocks: [...newPort]
+    })
+  }
+  
+  componentDidMount(){
+    fetch("http://localhost:3000/stocks")
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          allStocks: data
+        })
+      })
+  }
+
   render() {
+    console.log("state on render = ", this.state)
     return (
       <div>
-        <SearchBar/>
+        <SearchBar alphaChecked={this.state.alphaSort} priceChecked={this.state.priceSort}changeAlphaSort={this.changeAlphaSort} changeIndustrySort={this.changeIndustrySort} changePriceSort={this.changePriceSort}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer handleBuy={this.handleBuy} stocks={this.sortStocks()}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer handleSell={this.handleSell} stocks={this.state.portfolioStocks}/>
 
             </div>
           </div>
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,13 +3,15 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+  renderStocks = () => {
+    return this.props.stocks.map(stock => <Stock handleClick={this.props.handleBuy} key={stock.id} stock={stock}/>)
+  }
+
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.renderStocks()}
       </div>
     );
   }
