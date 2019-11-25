14197b3928fc8ebeb8b2ba75e30b4ec07d48b3db
Altaf Quadri <altafmquadri@gmail.com>
2019-11-24 22:19:29 -0500

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,31 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
+const Stock = (props) => {
 
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+  const stockClicker = () => {
+
+    props.addAStock ? props.addAStock(props.stock) : props.removeAStock(props.stock)
+    console.log(props.stock)
+  }
+
+  return (
+    <div>
+
+      <div onClick={stockClicker} className="card">
+        <div className="card-body">
+          <h5  className="card-title">{
+              //Company Name
+              props.stock.name
+            }</h5>
+          <p className="card-text">{
+              //ticker: stock price
+              props.stock.ticker + ' : ' + props.stock.price
+            }</p>
+        </div>
       </div>
-    </div>
 
 
-  </div>
-);
+    </div>)
+};
 
 export default Stock
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,22 +3,79 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+const api = 'http://localhost:3000/stocks'
+
 class MainContainer extends Component {
+
+  state = {
+    stocks: [],
+    addedStock: [],
+    sortedStocks: []
+  }
+
+  componentDidMount() {
+    fetch(api).then(res => res.json()).then(stocks => this.setState(
+      { 
+        stocks: stocks,
+        sortedStocks: stocks
+      }))
+  }
+
+  addAStock = (pickedStock) => {
+    this.setState({ addedStock:  [...this.state.addedStock, pickedStock ] });
+  }
+
+  removeAStock = (pickedStock) => {
+    this.setState({ addedStock: this.state.addedStock.filter(stock => stock.id !== pickedStock.id)  });
+    //how can you only remove one instance
+  }
+
+  filteredStocks = (value) => {
+    let fStocks = [...this.state.stocks]
+    let sortedStocks
+    switch(value) {
+      case 'Alphabetically':
+        sortedStocks = fStocks.sort((stockA, stockB) => {
+          if (stockA.name > stockB.name){
+            return 1
+          } else if (stockA.name < stockB.name) {
+            return -1
+          } else {
+            return 0
+          }
+        })
+        break;
+      case 'Price':
+        sortedStocks = fStocks.sort((stockA, stockB) => stockA.price - stockB.price)
+        break;
+      default:
+        sortedStocks = this.state.stocks
+        break;
+    }
+  this.setState({ sortedStocks: sortedStocks }); 
+}
+  
+  byTypeStocks = (value) => {
+    let sortedStocks = [...this.state.sortedStocks]
+    sortedStocks = sortedStocks.filter(stock => stock.type.includes(value))
+    this.setState({ sortedStocks: sortedStocks  });
+  }
+  
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar filteredStocks={this.filteredStocks} byTypeStocks={this.byTypeStocks}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.sortedStocks} addAStock={this.addAStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={this.state.addedStock} removeAStock={this.removeAStock}/>
 
             </div>
           </div>
